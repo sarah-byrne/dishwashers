fa43190c407e868dfb939a398a5e928156f25fea
Sarah Byrne <sarah.byrne@johnlewis.co.uk>
2019-06-06 15:17:27 +0100

    add title to ProductSummary

--- src/components/ProductSummary/ProductSummary.js
+++ src/components/ProductSummary/ProductSummary.js
@@ -3,6 +3,7 @@ import React from "react";
 const ProductSummary = props => (
   <div id={`dishwasherSummary${props.index}`}>
     <img id={`image${props.index}`} src={props.product.image} />
+    <div id={`title${props.index}`}>{props.product.title}</div>
     <div id={`price${props.index}`}>£{props.product.price.now}</div>
   </div>
 );
--- src/components/ProductSummary/ProductSummary.test.js
+++ src/components/ProductSummary/ProductSummary.test.js
@@ -7,7 +7,8 @@ import ProductSummary from "./ProductSummary";
 describe("ProductSummary", () => {
   const product = {
     price: { now: "10" },
-    image: "//johnlewis.scene7.com/is/image/JohnLewis/233326789?"
+    image: "//johnlewis.scene7.com/is/image/JohnLewis/233326789?",
+    title: "Indesit DIF 04B1 Ecotime Fully Integrated Dishwasher, White"
   };
   const summary = shallow(
     <ProductSummary product={product} key={0} index={0} />
@@ -22,6 +23,13 @@ describe("ProductSummary", () => {
     const prodPrice = summary.find("#price0");
 
     expect(prodPrice.text()).toEqual(`£${price}`);
+  });
+
+  it("displays the title", () => {
+    const title = "Indesit DIF 04B1 Ecotime Fully Integrated Dishwasher, White";
+    const prodTitle = summary.find("#title0");
+
+    expect(prodTitle.text()).toEqual(`${title}`);
   });
 
   it("displays the image", () => {
