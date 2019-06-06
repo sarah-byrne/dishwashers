ebecc207f9a777b7494a1f92081efe08d7aa4a6b
Sarah Byrne <sarah.byrne@johnlewis.co.uk>
2019-06-06 15:01:48 +0100

    add image to ProductSummary

--- src/components/ProductSummary/ProductSummary.js
+++ src/components/ProductSummary/ProductSummary.js
@@ -1,7 +1,10 @@
 import React from "react";
 
 const ProductSummary = props => (
-  <div id={`dishwasherSummary${props.index}`}>£{props.product.price.now}</div>
+  <div id={`dishwasherSummary${props.index}`}>
+    <img id={`image${props.index}`} src={props.product.image} />£
+    {props.product.price.now}
+  </div>
 );
 
 export default ProductSummary;
--- src/components/ProductSummary/ProductSummary.test.js
+++ src/components/ProductSummary/ProductSummary.test.js
@@ -5,15 +5,27 @@ import renderer from "react-test-renderer";
 import ProductSummary from "./ProductSummary";
 
 describe("ProductSummary", () => {
+  const product = {
+    price: { now: "10" },
+    image: "//johnlewis.scene7.com/is/image/JohnLewis/233326789?"
+  };
+  const summary = shallow(
+    <ProductSummary product={product} key={0} index={0} />
+  );
+  const displayedPrice = summary.find("#dishwasherSummary0");
+
   it("displays the price", () => {
-    const product = { price: { now: "10" } };
     const price = "10";
-    const summary = shallow(
-      <ProductSummary product={product} key={0} index={0} />
-    );
-    const displayedPrice = summary.find("#dishwasherSummary0");
 
     expect(summary).toMatchSnapshot();
     expect(displayedPrice.text()).toEqual(`£${price}`);
+  });
+
+  it("displays the image", () => {
+    const picture = summary.find("#image0");
+
+    expect(picture.prop("src")).toEqual(
+      "//johnlewis.scene7.com/is/image/JohnLewis/233326789?"
+    );
   });
 });
