e2f2264377056091f2bacfb9c3e27a4ee70f5c83
Sarah Byrne <sarah.byrne@johnlewis.co.uk>
2019-06-28 12:24:55 +0100

    call Product API and remove mocked data

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,29 @@
 import ProductGrid from "../src/components/ProductGrid/ProductGrid";
+import React from "react";
+import { Component } from "react";
+import fetch from "isomorphic-fetch";
 
-function Dishwashers() {
-  // Convert to react class when state required - API call
-  return (
-    <div>
-      <h1 id="dishWasherGrid">Dishwashers</h1>{" "}
-      {/* Convert to component that takes props of number of products */}
-      <ProductGrid />
-    </div>
-  );
+class Dishwashers extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  static async getInitialProps() {
+    const url =
+      "https://api.johnlewis.com/v1/products/search?q=dishwasher&key=Wu1Xqn3vNrd1p7hqkvB6hEu0G9OrsYGb&pageSize=20";
+    const res = await fetch(url);
+    const data = await res.json();
+    return { data };
+  }
+
+  render() {
+    return (
+      <div>
+        <h1 id="dishWasherGrid">Dishwashers</h1>{" "}
+        <ProductGrid products={this.props.data.products} />
+      </div>
+    );
+  }
 }
 
 export default Dishwashers;
